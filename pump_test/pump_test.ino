4c5e86ee8195586a893f4713b2a9018fd241059c
Zhenyu Yang <zhenyuyang@ucsb.edu>
2018-12-03 23:01:06 -0800

    added t o4 pumps

--- pump_test/pump_test.ino
+++ pump_test/pump_test.ino
@@ -7,6 +7,8 @@ int frameCount = 0;
 
 const int airPump1 = 9;    //Air pump motor with npn transistor at pin 9 of Arduino
 const int airPump2 = 10;    //Air pump motor with npn transistor at pin 9 of Arduino
+const int airPump3 = 11;    //Air pump motor with npn transistor at pin 9 of Arduino
+const int airPump4 = 6;    //Air pump motor with npn transistor at pin 9 of Arduino
 
 const int MPU=0x68;
 int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
@@ -20,22 +22,41 @@ void setup(){
 Serial.begin(9600);
 pinMode(airPump1,OUTPUT);
 pinMode(airPump2,OUTPUT);
-
+pinMode(airPump3,OUTPUT);
+pinMode(airPump4,OUTPUT);
+airPumpOutput  = 255;
 }
 
 
 
 void loop(){
 
-airPumpOutput  = 255;
-analogWrite(airPump1,airPumpOutput);
-analogWrite(airPump2,255-airPumpOutput);
+
+analogWrite(airPump1,0);
+analogWrite(airPump2,255);
+analogWrite(airPump3,255);
+analogWrite(airPump4,255);
 //Serial.print("airPumpOutput = ");Serial.println(airPumpOutput);
 delay(1000); 
 
-airPumpOutput = 0;
-analogWrite(airPump1,airPumpOutput);
-analogWrite(airPump2,255-airPumpOutput);
+analogWrite(airPump1,255);
+analogWrite(airPump2,0);
+analogWrite(airPump3,255);
+analogWrite(airPump4,255);
+//Serial.print("airPumpOutput = ");Serial.println(airPumpOutput);
+delay(1000);
+
+analogWrite(airPump1,255);
+analogWrite(airPump2,255);
+analogWrite(airPump3,0);
+analogWrite(airPump4,255);
+//Serial.print("airPumpOutput = ");Serial.println(airPumpOutput);
+delay(1000); 
+
+analogWrite(airPump1,255);
+analogWrite(airPump2,255);
+analogWrite(airPump3,255);
+analogWrite(airPump4,0);
 //Serial.print("airPumpOutput = ");Serial.println(airPumpOutput);
 delay(1000); 
 }
